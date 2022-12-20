63eab23f04006ef76e9061cc715bd20b9cdd9f59
kar-nguyen <karine_nguyen@hotmail.com>
2022-12-19 22:46:46 -0500

    words extraction implemented + added missing '/' in txt

--- main.py
+++ main.py
@@ -1,29 +1,96 @@
+# returns a list of 2 lists (lists of words and lists of categories)
 def preprocess_file(file_name):
-    data = []   # will contain [[words], [categories]] for each line
+    data = []
+    word_lists = []
+    category_lists = []
     with open(file_name) as f:
         lines = f.readlines()
         for line in lines:
             if line[:2] != '$$':
-                # remove whitespace and punctuation
-                current_line = line.replace('=', '')
-                current_line = current_line.replace(',/,', '')
-                current_line = current_line.replace('``/``', '')
-                current_line = current_line.replace('[', '')
-                current_line = current_line.replace(']', '')
-                current_line = current_line.replace('\n', '')
-                current_line = current_line.replace('/', ' ')
-                current_line = list(filter(None, current_line.split(' ')))  # removes all the whitespaces in line
+                current_line = remove_line_filler(line)
 
                 # seperate words and categories in 2 lists
                 word_list = current_line[::2]
                 category_list = current_line[1::2]
 
-                words_categories_data = [word_list, category_list]
-                data.append(words_categories_data)
+                # words_categories_data = [word_list, category_list]
+                # data.append(words_categories_data)
+                word_lists.append(word_list)
+                category_lists.append(category_list)
+
+        data.append(word_lists)
+        data.append(category_lists)
 
     return data
 
 
-# Press the green button in the gutter to run the script.
+# function that removes whitespaces and other characters
+def remove_line_filler(line):
+    current_line = line.replace('=', '')
+    current_line = current_line.replace(',/,', '')
+    current_line = current_line.replace('[', '')
+    current_line = current_line.replace(']', '')
+    current_line = current_line.replace('\n', '')
+    current_line = current_line.replace('/', ' ')
+    return list(filter(None, current_line.split(' ')))  # removes all the whitespaces in line
+
+
+# extracts features depending on the context_information_type (words or category, extra the 2 contexts before and after
+# the index
+# TODO: implement category extraction, same logic as words extraction
+def extract_features(file, context_information_type):
+    words_list = file[0]
+    words_before_after_idx = []
+    sense = []  # TODO: extract interest_ and append to sense list
+
+    if context_information_type == 'words':
+        for x in words_list:
+
+            interest_idx = index_lookup(x, 'interest')
+            context_before_idx = []
+            context_after_idx = []
+
+            # smallest_idx: idx du mot - 2 positions avant
+            smallest_idx = interest_idx - 2
+            if smallest_idx <= 0:
+                # start the count at the beginning of the list
+                smallest_idx = 0
+                while smallest_idx < interest_idx:
+                    context_before_idx.append(x[smallest_idx])
+                    smallest_idx += 1
+            else:
+                while smallest_idx < interest_idx:
+                    context_before_idx.append(x[smallest_idx])
+                    smallest_idx += 1
+
+            # bigger_idx: idx du mot + 2 positions apres
+            bigger_idx = interest_idx + 2
+            if bigger_idx >= len(x):
+                bigger_idx = len(x) - 1
+                while bigger_idx > interest_idx:
+                    context_after_idx.append(x[bigger_idx])
+                    bigger_idx -= 1
+            else:
+                while bigger_idx > interest_idx:
+                    context_after_idx.append(x[bigger_idx])
+                    bigger_idx -= 1
+
+            extracted_outputs = context_before_idx + context_after_idx
+            print(extracted_outputs)
+            words_before_after_idx.append(extracted_outputs)
+
+    return words_before_after_idx
+
+
+# function that returns the idx for a given word
+def index_lookup(line, word):
+    for x, elem in enumerate(line):
+        if word in elem:
+            return x
+    return None
+
+
 if __name__ == '__main__':
-    preprocess_file('interest.acl94.txt')
+    file = preprocess_file('interest.acl94.txt')
+    extract_features(file, 'words')
+    # extract_features(file, 'categories')
