82f7d71c0deb1d13bdcda004eb31e26143637581
kar-nguyen <karine_nguyen@hotmail.com>
2022-12-19 23:59:33 -0500

    implemented extract_features -> return categories log

--- main.py
+++ main.py
@@ -35,71 +35,92 @@ def remove_line_filler(line):
     return list(filter(None, current_line.split(' ')))  # removes all the whitespaces in line
 
 
-# extracts features depending on the context_information_type (words or category, extra the 2 contexts before and after
-# the index
-# TODO: implement category extraction, same logic as words extraction, feel free to rename variables!!
+# extracts features depending on the context_information_type (words or category)
+# TODO: feel free to rename variables!
 def extract_features(file, context_information_type):
     words_list = file[0]
+    categories_list = file[1]
+
     words_before_after_idx = []
-    sense = []  # TODO: extract interest_ and append to sense list
+    categories_before_after_idx = []
+
+    # contains all the interest_ or interests_
+    interests_list = []
+
+    for x, y in zip(words_list, categories_list):
+
+        words_before_idx = []
+        words_after_idx = []
+        categories_before_idx = []
+        categories_after_idx = []
+
+        interest_idx, interest_sense = index_lookup(x, 'interest')
+
+        interests_list.append(interest_sense)
+
+        # smallest_idx: idx du mot - 2 positions avant
+        smallest_idx = interest_idx - 2
+
+        """
+        if we're unable to keep 2 words/categories before the idx (out of bound), start at the beginning of the list 
+        at idx 0 and append the words/categories until we reach the idx, else append 2 words/categories 
+        before the idx as per usual
+        """
+        if smallest_idx <= 0:
+            smallest_idx = 0
+            while smallest_idx < interest_idx:
+                words_before_idx.append(x[smallest_idx])
+                categories_before_idx.append(y[smallest_idx])
+                smallest_idx += 1
+        else:
+            while smallest_idx < interest_idx:
+                words_before_idx.append(x[smallest_idx])
+                categories_before_idx.append(y[smallest_idx])
+                smallest_idx += 1
+
+        # bigger_idx: idx du mot + 2 positions apres
+        bigger_idx = interest_idx + 2
+
+        """
+        if we're unable to keep 2 words/categories after the idx (out of bound), start at the end of the list at 
+        len(list) and append the words/categories until we reach the idx, else append 2 words/categories 
+        after the idx as per usual
+        """
+        if bigger_idx >= len(x):
+            bigger_idx = len(x) - 1
+            while bigger_idx > interest_idx:
+                words_after_idx.append(x[bigger_idx])
+                categories_after_idx.append(y[bigger_idx])
+                bigger_idx -= 1
+        else:
+            while bigger_idx > interest_idx:
+                words_after_idx.append(x[bigger_idx])
+                categories_after_idx.append(y[bigger_idx])
+                bigger_idx -= 1
+
+        extracted_word_outputs = words_before_idx + words_after_idx
+        extracted_categories_outputs = categories_before_idx + categories_after_idx
+
+        categories_before_after_idx.append(extracted_categories_outputs)
+        words_before_after_idx.append(extracted_word_outputs)
 
     if context_information_type == 'words':
-        for x in words_list:
-
-            interest_idx = index_lookup(x, 'interest')
-            context_before_idx = []
-            context_after_idx = []
-
-            # smallest_idx: idx du mot - 2 positions avant
-            smallest_idx = interest_idx - 2
-
-            """
-            if we're unable to keep 2 words before the idx (out of bound), start at the beginning of the list at idx 0
-            and append the words until we reach the idx, else append 2 words before the idx as per usual
-            """
-            if smallest_idx <= 0:
-                smallest_idx = 0
-                while smallest_idx < interest_idx:
-                    context_before_idx.append(x[smallest_idx])
-                    smallest_idx += 1
-            else:
-                while smallest_idx < interest_idx:
-                    context_before_idx.append(x[smallest_idx])
-                    smallest_idx += 1
-
-            # bigger_idx: idx du mot + 2 positions apres
-            bigger_idx = interest_idx + 2
-
-            """
-            if we're unable to keep 2 words after the idx (out of bound), start at the end of the list at len(list)
-            and append the words until we reach the idx, else append 2 words after the idx as per usual
-            """
-            if bigger_idx >= len(x):
-                bigger_idx = len(x) - 1
-                while bigger_idx > interest_idx:
-                    context_after_idx.append(x[bigger_idx])
-                    bigger_idx -= 1
-            else:
-                while bigger_idx > interest_idx:
-                    context_after_idx.append(x[bigger_idx])
-                    bigger_idx -= 1
-
-            extracted_outputs = context_before_idx + context_after_idx
-            print(extracted_outputs)
-            words_before_after_idx.append(extracted_outputs)
-
-    return words_before_after_idx
+        return words_before_after_idx
+    elif context_information_type == 'categories':
+        return categories_before_after_idx
 
 
-# function that returns the idx for a given word
+# function that returns the idx and full string given a substring (interest)
 def index_lookup(line, word):
     for x, elem in enumerate(line):
         if word in elem:
-            return x
+            return [x, elem]
     return None
 
 
 if __name__ == '__main__':
+
+    # TODO: a retirer, pour tester
     file = preprocess_file('interest.acl94.txt')
     extract_features(file, 'words')
-    # extract_features(file, 'categories')
+    extract_features(file, 'categories')
