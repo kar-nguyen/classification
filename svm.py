73f36fc21f626cd685a692d728d9c04cb8d4cbfb
Bianca Bica <bianca.bica@umontreal.ca>
2022-12-23 22:30:05 -0500

    Changement de certains param

--- svm.py
+++ svm.py
@@ -12,17 +12,16 @@ def svm_classifer(file, context_information_type):
                                                                         random_state=50)
     rbf = svm.SVC(kernel='rbf', gamma=0.5, C=0.1).fit(X_train, Y_train)
 
-    #TODO : peut-etre choisir d'autres degres pour les polynomes jsp
-    poly1 = svm.SVC(kernel='poly', degree=1, C=1).fit(X_train, Y_train)
     poly2 = svm.SVC(kernel='poly', degree=2, C=1).fit(X_train, Y_train)
+    poly3 = svm.SVC(kernel='poly', degree=3, C=1).fit(X_train, Y_train)
     poly5 = svm.SVC(kernel='poly', degree=5, C=1).fit(X_train, Y_train)
 
 
     #linear = LinearSVC(C=1, multi_class='ovr', max_iter=5000).fit(X_train, Y_train)
 
     rbf_pred = rbf.predict(X_test)
-    poly1_pred = poly1.predict(X_test)
     poly2_pred = poly2.predict(X_test)
+    poly3_pred = poly3.predict(X_test)
     poly5_pred = poly5.predict(X_test)
 
     #lin_pred = linear.predict(X_test)
@@ -30,16 +29,16 @@ def svm_classifer(file, context_information_type):
     rbf_accuracy = accuracy_score(Y_test, rbf_pred)
     print('Accuracy (RBF Kernel): ', "%.2f" % (rbf_accuracy * 100))
 
-    poly1_accuracy = accuracy_score(Y_test, poly1_pred)
-    print('Accuracy (Polynomial Degree 1 Kernel): ', "%.2f" % (poly1_accuracy * 100))
-
-
     poly2_accuracy = accuracy_score(Y_test, poly2_pred)
     print('Accuracy (Polynomial Degree 2 Kernel): ', "%.2f" % (poly2_accuracy * 100))
 
 
+    poly3_accuracy = accuracy_score(Y_test, poly3_pred)
+    print('Accuracy (Polynomial Degree 3 Kernel): ', "%.2f" % (poly3_accuracy * 100))
+
+
     poly5_accuracy = accuracy_score(Y_test, poly5_pred)
-    print('Accuracy (Polynomial Degree 3 Kernel): ', "%.2f" % (poly5_accuracy * 100))
+    print('Accuracy (Polynomial Degree 5 Kernel): ', "%.2f" % (poly5_accuracy * 100))
 
     #lin_accuracy = accuracy_score(Y_test, lin_pred)
     #print('Accuracy (Linear Kernel): ', "%.2f" % (lin_accuracy * 100))
